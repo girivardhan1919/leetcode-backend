5fde21e000d1522e3ca895300bedbc45486feb9b
girivardhan1919 <daggupati.girivardhan@gmail.com>
2024-03-25 00:50:42 +0530

    added routing struture for versioned api for problems

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/server.config');
+const apiRouter = require('./routes');
 
 const app = express();
 
@@ -8,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+app.use('/api', apiRouter);
 app.get('/ping', (req, res) => {
   return res.json({ message: 'Problem service is live' });
 });
