7937805d52ed1f6489b3dd13cb6b2f965721a3cf
girivardhan1919 <daggupati.girivardhan@gmail.com>
2024-03-25 15:42:59 +0530

    added exception handling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/server.config');
 const apiRouter = require('./routes');
+const errorHandler = require('./utils/errorHandler');
 
 const app = express();
 
@@ -13,6 +14,9 @@ app.use('/api', apiRouter);
 app.get('/ping', (req, res) => {
   return res.json({ message: 'Problem service is live' });
 });
+
+// last middleware if any error comes
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server started at PORT: ${PORT}`);
